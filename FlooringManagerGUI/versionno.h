d1458faa56be7d17011a7c98abf976df8c4e2f5c
Robert Raboud <robert.raboud@outlook.com>
2014-12-11 17:14:33 +0000

    Originally checked in by TFSSERVER\rraboud on 2012-12-13 10:07:20.653 as Changeset 915
    git-tfs-id: [https://randr.visualstudio.com]$/IMClassic/Main/src;C2724

--- FlooringManagerGUI/versionno.h
+++ FlooringManagerGUI/versionno.h
@@ -6,13 +6,13 @@
 
 // set generic version info
 #define VINFO_HI_FILEVERSION 			03
-#define VINFO_LO_FILEVERSION 			27
-#define VINFO_HI_FILEBUILD   			00
+#define VINFO_LO_FILEVERSION 			26
+#define VINFO_HI_FILEBUILD   			01
 #define VINFO_LO_FILEBUILD   			00
 
 #define VINFO_HI_PRODUCTVERSION 		03
-#define VINFO_LO_PRODUCTVERSION 		27
-#define VINFO_HI_PRODUCTBUILD   		00
+#define VINFO_LO_PRODUCTVERSION 		26
+#define VINFO_HI_PRODUCTBUILD   		01
 #define VINFO_LO_PRODUCTBUILD   		00
 
 #define VINFO_STRING_COMMENTS			"Installation Manager Classic\000"
