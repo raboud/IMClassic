e01abaf9bb10da870eea0f5cf7e66775d80ec7eb
Robert Raboud <robert.raboud@outlook.com>
2014-12-11 17:04:47 +0000

    Originally checked in by TFSSERVER\rraboud on 2012-09-30 22:12:56.63 as Changeset 859
    git-tfs-id: [https://randr.visualstudio.com]$/IMClassic/Main/src;C2672

--- FlooringManagerGUI/versionno.h
+++ FlooringManagerGUI/versionno.h
@@ -1,22 +1,8 @@
 /*----------------------------------------------------------------------
-	Copyright © 2001-2012 JRS Technology, Inc.
+	Copyright © 2001-2012 R & R Engineering Technology, Inc.
 -----------------------------------------------------------------------*/
 #ifndef _VERSIONNO_H
 #define _VERSIONNO_H
-
-#ifdef _DEBUG
-	#ifdef _BETA
-		#define VINFO_VERSIONFLAGS				VS_FF_DEBUG | VS_FF_PRERELEASE
-	#else
-		#define VINFO_VERSIONFLAGS				VS_FF_DEBUG
-	#endif
-#else
-	#ifdef _BETA
-		#define VINFO_VERSIONFLAGS				VS_FF_PRERELEASE
-	#else
-		#define VINFO_VERSIONFLAGS				0x0
-	#endif
-#endif
 
 // set generic version info
 #define VINFO_HI_FILEVERSION 			03
@@ -30,7 +16,7 @@
 #define VINFO_LO_PRODUCTBUILD   		300
 
 #define VINFO_STRING_COMMENTS			"Installation Manager Classic\000"
-#define VINFO_STRING_COMPANYNAME		"Custom Installations, Inc.\000"
+#define VINFO_STRING_COMPANYNAME		"R & R Engineering, Inc.\000"
 #define VINFO_STRING_COPYRIGHT			"Copyright © 2001-2012, Custom Installations, Inc.\000"
 #define VINFO_STRING_FILEDESCRIPTION	"Installation Manager Classic\000"
 #define VINFO_STRING_PRODUCTNAME		"Installation Manager Classic\000"
@@ -40,22 +26,35 @@
 // set to filename with exe
 #define VINFO_STRING_ORIGINALNAME		"flooring.exe\000"
 
+// Create strings to be used in the version strings
+#define FILEVERSION_STRING			MAKE_STRING(VINFO_HI_FILEVERSION) "." MAKE_STRING(VINFO_LO_FILEVERSION) "." MAKE_STRING( VINFO_HI_FILEBUILD ) "." MAKE_STRING( VINFO_LO_FILEBUILD )
+#define PRODUCTVERSION_STRING		MAKE_STRING(VINFO_HI_PRODUCTVERSION) "." MAKE_STRING(VINFO_LO_PRODUCTVERSION) "." MAKE_STRING( VINFO_HI_PRODUCTBUILD ) "." MAKE_STRING( VINFO_LO_PRODUCTBUILD )
+
+// Macro to create strings from non string 
+// Note we need the two levels here so it works when passed a macro value 
+#define INT_MAKE_STRING(in) #in
+#define MAKE_STRING(in) INT_MAKE_STRING( in )
+
 // set version stuff according to debug
 	#ifdef _DEBUG
 		#ifdef _BETA
-			#define VINFO_STRING_FILEVERSION		"3.20 (Debug Beta Build 300)\000"
-			#define VINFO_STRING_PRODUCTVERSION		"3.20 (Debug Beta Build 300)\000"
+			#define VINFO_VERSIONFLAGS				VS_FF_DEBUG | VS_FF_PRERELEASE
+			#define VINFO_STRING_FILEVERSION		FILEVERSION_STRING "  Debug Beta\000"
+			#define VINFO_STRING_PRODUCTVERSION		PRODUCTVERSION_STRING "  Debug Beta\000"
 		#else
-			#define VINFO_STRING_FILEVERSION		"3.20 (Debug Build 300)\000"
-			#define VINFO_STRING_PRODUCTVERSION		"3.20 (Debug Build 300)\000"
+			#define VINFO_VERSIONFLAGS				VS_FF_DEBUG
+			#define VINFO_STRING_FILEVERSION		FILEVERSION_STRING "  Debug\000"
+			#define VINFO_STRING_PRODUCTVERSION		PRODUCTVERSION_STRING "  Debug\000"
 		#endif
 	#else
 		#ifdef _BETA
-			#define VINFO_STRING_FILEVERSION		"3.20 (Release Beta Build 300)\000"
-			#define VINFO_STRING_PRODUCTVERSION		"3.20 (Release Beta Build 300)\000"
+			#define VINFO_VERSIONFLAGS				VS_FF_PRERELEASE
+			#define VINFO_STRING_FILEVERSION		FILEVERSION_STRING "  Release Beta\000"
+			#define VINFO_STRING_PRODUCTVERSION		PRODUCTVERSION_STRING "  Release Beta\000"
 		#else
-			#define VINFO_STRING_FILEVERSION		"3.20 (Release Build 300)\000"
-			#define VINFO_STRING_PRODUCTVERSION		"3.20 (Release Build 300)\000" 
+			#define VINFO_VERSIONFLAGS				0x0
+			#define VINFO_STRING_FILEVERSION		FILEVERSION_STRING "  Release\000"
+			#define VINFO_STRING_PRODUCTVERSION		PRODUCTVERSION_STRING "  Release\000"
 		#endif
 	#endif
 #endif
