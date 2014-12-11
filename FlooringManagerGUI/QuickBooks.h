dc8e60a5b3f378c95b19ab99aaf116b2fae464e4
Robert Raboud <robert.raboud@outlook.com>
2014-12-11 18:05:55 +0000

    Originally checked in by TFSSERVER\rraboud on 2013-11-13 15:33:44.857 as Changeset 1189
    git-tfs-id: [https://randr.visualstudio.com]$/IMClassic/Main/src;C2927

--- FlooringManagerGUI/QuickBooks.h
+++ FlooringManagerGUI/QuickBooks.h
@@ -6,38 +6,30 @@
 
 #include "setpayroll.h"	// Added by ClassView
 
-//const char szREGISTRY_KEY_QBSTRINGS[] = "Quickbooks Settings";
 const char szSETTINGS_VALUE_QBFILENAME[] = "QBQuickbooksFilename";
 const char szSETTINGS_VALUE_ACCOUNTSPAYABLEACCOUNT[] = "QBAccountsPayableAccount";
 const char szSETTINGS_VALUE_LABORSUBCONTRACTACCOUNT[] = "QBLaborSubcontractAccount";
 const char szSETTINGS_VALUE_RETAINAGEACCOUNT[] = "QBRetainageAccount";
 const char szSETTINGS_VALUE_SAVINGSACCOUNT[] = "QBSavingsAccount";
-//const char szSETTINGS_VALUE_SOFTSURFACECLASS[] = "QBSoftSurfaceClass";
-//const char szSETTINGS_VALUE_HARDSURFACECLASS[] = "QBHardSurfaceClass";
 const char szSETTINGS_VALUE_COMMERCIALCLASS[] = "QBCommercialClass";
 const char szSETTINGS_VALUE_RETAINAGECLASS[] = "QBRetainageClass";
 const char szSETTINGS_VALUE_SAVINGSCLASS[] = "QBSavingsClass";
 const char szSETTINGS_VALUE_OVERHEADCLASS[] = "QBOverheadClass";
 const char szSETTINGS_VALUE_CABINETSCLASS[] = "QBCabinetsClass";
-//const char szSETTINGS_VALUE_MEASURECLASS[] = "QBMeasureClass";
 const char szSETTINGS_VALUE_WORKMANSCOMPACCOUNT[] = "QBWorkersCompAccount";
 const char szSETTINGS_VALUE_WORKMANSCOMPCLASS[] = "QBWorkersCompClass";
 const char szSETTINGS_VALUE_OUTPUTFOLDER[] = "QBOutputFolder";
-
 
 const char szSETTINGS_VALUE_QBFILENAME_DEFAULT[] = "Sub-Contractors.iif";
 const char szSETTINGS_VALUE_ACCOUNTSPAYABLEACCOUNT_DEFAULT[] = "Accounts Payable";
 const char szSETTINGS_VALUE_LABORSUBCONTRACTACCOUNT_DEFAULT[] = "Labor Subcontract";
 const char szSETTINGS_VALUE_RETAINAGEACCOUNT_DEFAULT[] = "Retainage - Long Term";
 const char szSETTINGS_VALUE_SAVINGSACCOUNT_DEFAULT[] = "Savings";
-//const char szSETTINGS_VALUE_SOFTSURFACECLASS_DEFAULT[] = "Home Depot:Soft Surface";
-//const char szSETTINGS_VALUE_HARDSURFACECLASS_DEFAULT[] = "Home Depot:Hard Surface";
 const char szSETTINGS_VALUE_COMMERCIALCLASS_DEFAULT[] = "Commercial";
 const char szSETTINGS_VALUE_RETAINAGECLASS_DEFAULT[] = "Retainage";
 const char szSETTINGS_VALUE_SAVINGSCLASS_DEFAULT[] = "Savings";
 const char szSETTINGS_VALUE_OVERHEADCLASS_DEFAULT[] = "Overhead";
 const char szSETTINGS_VALUE_CABINETSCLASS_DEFAULT[] = "Home Depot:Cabinets";
-//const char szSETTINGS_VALUE_MEASURECLASS_DEFAULT[] = "Home Depot:Measures";
 const char szSETTINGS_VALUE_WORKMANSCOMPACCOUNT_DEFAULT[] = "Workerscomp";
 const char szSETTINGS_VALUE_WORKMANSCOMPCLASS_DEFAULT[] = "Workerscomp";
 const char szSETTINGS_VALUE_OUTPUTFOLDER_DEFAULT[] = "C:\\temp\\";
